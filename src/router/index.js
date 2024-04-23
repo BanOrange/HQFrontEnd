4d9eaf3a0ec9f40e3af0da395c7f3aa95e2b42e3
wesker1121 <143385727+wesker1121@users.noreply.github.com>
2024-04-23 20:57:07 +0800

    LoginView.vue,数据库端口8888

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,53 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import HomeView from '../views/HomeView.vue'
+import StudentView from "../views/StudentView.vue";
+import TeacherView from "../views/TeacherView.vue";
+import AdminView from "../views/AdminView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView
-    },
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
 
-  ]
+        {
+            path: '/student',
+            name: 'student',
+            component: StudentView
+        },
+        {
+            path: '/teacher',
+            name: 'teacher',
+            component: TeacherView
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: AdminView
+        }
+
+    ]
 })
 
-router.beforeEach((to,from,next)=>{
-  if(to.path == '/login'){
-    next()
-  }else{
-    const username = sessionStorage.getItem("username")
-    if(username == null){
-      next('/login')
-    }else{
-      next();
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') {
+        next()
+    } else {
+        const username = sessionStorage.getItem("username")
+        if (username == null) {
+            next('/login')
+        } else {
+            next();
+        }
     }
-  }
 })
 export default router
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,51 @@
-<template>
-    <div>
-    <el-input v-model="username" style="width: 240px" placeholder="请输入用户名" />
-    </div>
-    <el-input v-model="password" type="password" style="width: 240px" placeholder="请输入密码"/><br>
-    <el-button type="primary" @click ="handleLogin">登录</el-button>
-</template>
-
 <script setup>
-import { ref } from 'vue';
+import {ref} from 'vue';
+import {useRouter} from "vue-router";
+import HomeView from '../views/HomeView.vue'
+import LoginView from "../views/LoginView.vue";
+
+import axios from 'axios'
+import qs from 'qs'
+import {ElMessage} from 'element-plus'
+
 let username = ref("");
 let password = ref("");
+let userType = ref("student");
+const router = useRouter();
 
+function handleClick() {
+  let data = {
+    username: username.value,
+    password: password.value,
+    userType: userType.value
+  }
+  axios.post('http://localhost:8888/login', qs.stringify(data))
+      .then((res) => {
+        if (res.data.code === 200) {
+          sessionStorage.setItem("username", username.value)
+          router.replace(userType.value === 'student' ? '/student' : userType.value === 'teacher' ? '/teacher' : '/admin')
+          console.log(res.data.msg)
+        } else {
+          ElMessage.error(res.data.msg)
+        }
+      })
+}
 
 </script>
+
+<template>
+  <div>
+    <el-input v-model="username" style="width: 240px" placeholder="请输入用户名"/>
+    <br><br>
+    <el-input v-model="password" type="password" style="width: 240px" placeholder="请输入密码"/>
+    <br><br>
+    <el-text>请选择用户类型</el-text><br>
+    <el-select v-model="userType" style="width: 240px" placeholder="请选择用户类型">
+      <el-option label="学生" value="student"></el-option>
+      <el-option label="教师" value="teacher"></el-option>
+      <el-option label="管理员" value="admin"></el-option>
+    </el-select>
+    <br>
+    <el-button type="primary" @click="handleClick">登录</el-button>
+  </div>
+</template>
