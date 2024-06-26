bb7ab7c09ad365660bee341214e05bc77150569b
wesker1121 <143385727+wesker1121@users.noreply.github.com>
2024-06-26 10:12:29 +0800

    Update teacherSearchGrade

--- src/components/teacher/SearchGradeView.vue
+++ src/components/teacher/SearchGradeView.vue
@@ -6,11 +6,25 @@ import {ElMessage} from 'element-plus';
 import {onMounted} from 'vue';
 import router from '@/router';
 
+
+
 const tableData = ref([])
 const form = reactive({
   name: '',
   id: '',
 })
+
+let checkList = [];
+const Select = (index) => {
+  if (checkList.includes(index)) {
+    checkList.splice(checkList.indexOf(index), 1);
+  } else {
+    checkList.push(index);
+  }
+  console.log(checkList);
+}
+
+
 onMounted(() => {
   getAllGrade();
 })
@@ -26,14 +40,43 @@ function getAllGrade() {
       })
 }
 
+function handleSearch() {
+  let data = {
+    id: form.id,
+    name: form.name,
+  }
+
+  axios.post("http://localhost:8080/searchCourse", qs.stringify(data))
+      .then((res) => {
+        if (res.data.code === 200) {
+          ElMessage("查询成功")
+          tableData.value = res.data;
+
+        } else {
+          ElMessage.error(res.data.msg)
+        }
+      })
+}
+
 </script>
+
 
 <template>
   <h1>教师成绩查询</h1>
+  <el-form :model="form">
+    <el-form-item label="课程编号">
+      <el-input v-model="form.id"></el-input>
+    </el-form-item>
+    <el-form-item label="课程名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="handleSearch">查询</el-button>
+    </el-form-item>
+  </el-form>
   <el-table :data="tableData" width="400px" max-height="200">
-    <el-table-column fixed prop="name" label="课程名称" width="150"/>
-    <el-table-column prop="id" label="课程编号" width="120"/>
-    <el-table-column prop="name" label="教师" width="120"/>
+    <el-table-column fixed prop="studentName" label="学员名称" width="150"/>
+    <el-table-column prop="studentId" label="学员编号" width="120"/>
     <el-table-column prop="grade" label="成绩" width="120"/>
     <el-table-column prop="Credit" label="学分" width="120"/>
   </el-table>
