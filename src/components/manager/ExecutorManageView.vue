b1e6f20a08d53a7d94dd3d34a92ed1394b92ed06
wesker1121 <143385727+wesker1121@users.noreply.github.com>
2024-07-04 19:32:23 +0800

    修改经理看执行人

--- src/components/manager/ExecutorManageView.vue
+++ src/components/manager/ExecutorManageView.vue
@@ -4,36 +4,51 @@ import axios from 'axios';
 import qs from 'querystring';
 import {ElMessage} from 'element-plus';
 import {onMounted} from 'vue';
-import { useRouter,useRoute } from 'vue-router';
+import router from "@/router/index.js";
 
-let router = useRouter();
+
+//let router = useRouter();
 //用来装执行人的信息
-const tableData = ref([])
+
 
 //用来装搜索条件
 const searchForm = reactive({
-  name: '',
-  id: '',
+  exe_name: '',
+  exe_id: '',
+  username: '',
+})
+
+const tableData = ref([])
+
+onMounted(() =>{
+  findAllExecutor()
 })
 
 //找到所有的执行人信息
 function findAllExecutor(){
-  axios.get('http://localhost:8080/manager/findAllExecutor')
+  let username = sessionStorage.getItem("username")
+  let data = qs.stringify({
+    username: username,
+  });
+  console.log(username);
+  axios.post('http://localhost:8080/manager/findAllExecutor', data) //get改成post
   .then((res)=>{
+    console.log(res.data);
     tableData.value = res.data;
+  })
+  .catch((error)=>{
+    console.error(error);
   })
 }
 
 //挂载,但是为了方便开发，先注释掉
-onMounted(() =>{
-//   findAllExecutor();
-})
+
 
 //查询执行人功能，将符合条件的执行人列出来
 function handleSearch() {
   let data = {
-    executorID: searchForm.executorID,
-    executorName: searchForm.executorName,
+    executorID: searchForm.exe_id,
+    executorName: searchForm.exe_name,
   }
 
   axios.post("http://localhost:8080/manager/searchExecutor", qs.stringify(data))
@@ -45,25 +60,25 @@ function handleSearch() {
 </script>
 
 <template>
-    <el-form :model="searchForm" label-width="auto" style="max-width: 300px">
-    <h2>执行人查询</h2><br>
-    <el-form-item label="执行人工号：">
-      <el-input v-model="searchForm.id"/>
+  <h2>执行人查询</h2><br>
+  <el-form :model="searchForm" label-width="auto" style="max-width: 300px">
+    <el-form-item label="执行人ID：">
+      <el-input v-model="searchForm.exe_id" style="width:200px"></el-input>
     </el-form-item>
     <el-form-item label="执行人姓名：">
-      <el-input v-model="searchForm.name"/>
+      <el-input v-model="searchForm.exe_name" style="width:200px"></el-input>
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="handleSearch">查询</el-button>
     </el-form-item>
   </el-form>
   <br><br>
-
+<h1>执行人个人信息</h1>
   <br>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column fixed prop="id" label="工号" width="150" />
-    <el-table-column prop="name" label="姓名" width="120" />
-    <el-table-column prop="state" label="工作状态" width="120" />
+  <el-table :data="tableData" width="400px" max-height="200">
+    <el-table-column fixed prop="exe_id" label="工号" width="200" />
+    <el-table-column prop="exe_name" label="姓名" width="200" />
+    <el-table-column prop="username" label="用户名" width="200" /> //可以返回用户名吗？
   </el-table>
 </template>
 
